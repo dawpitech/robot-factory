10bb0ebfce238b39775b034b8ca5b19fa60d7bf0
ALittlePatate <maxime@patate.dev>
2024-03-04 17:59:23 +0100

    [ADD] indexes to compiler, [SEMI-FIX] byte encoding

--- bonus/jon.s
+++ bonus/jon.s
@@ -2,6 +2,4 @@
         .comment "Winter is coming"
 
         sti r1, %:crow, %1
-crow:   live %234
-        ld %0, r3
-        zjmp %:crow
+crow:   live %234 
--- sources/compiler/compiler.c
+++ sources/compiler/compiler.c
@@ -5,6 +5,7 @@
 ** compiler header
 */
 
+#include <endian.h>
 #include <stdio.h>
 #include <sys/types.h>
 
@@ -21,50 +22,54 @@ void compute_coding_byte(arg_list_t *args, assm_cfg_t *assm_cfg)
     write_byte(coding_byte, assm_cfg);
 }
 
-u_int8_t get_bytes(int num, int byteNumber)
+static
+void get_bytes(int num, int byte_nb, assm_cfg_t *assm_cfg)
 {
-    u_int8_t result = 0;
+    char extracted_byte = 0;
 
-    if (byteNumber < 1)
-        return 0;
-    for (int i = 0; i < byteNumber; ++i) {
-        result |= (u_int8_t)(num & 0xFF) << (8 * i);
-        num >>= 8;
+    num = byte_nb == 1 ? num : htobe32(num);
+    if (byte_nb < 1)
+        return;
+    for (int i = 0; i < byte_nb; i++) {
+        extracted_byte = (num >> (8 * i)) & 0xFF;
+        write_byte(extracted_byte, assm_cfg);
     }
-    return result;
 }
 
-int compute_arguments(arg_list_t *args, assm_cfg_t *assm_cfg)
+int compute_arguments(arg_list_t *args, assm_cfg_t *assm_cfg, int idx)
 {
-    u_int8_t curr_arg = 0;
     int nb = 0;
 
-    for (int i = 0; i < MAX_ARGS_NUMBER; i += 1) {
-        if (args[i].data == NULL)
-            continue;
+    for (int i = 0; i < MAX_ARGS_NUMBER && args[i].data != NULL; i += 1) {
         nb = my_getnbr(args[i].data);
         switch ((int)args[i].type) {
         case DIRECT:
-            curr_arg = get_bytes(nb, DIR_SIZE);
+            get_bytes(nb, idx ? IND_SIZE : DIR_SIZE, assm_cfg);
             break;
         case REGISTER:
-            curr_arg = get_bytes(nb, REG_SIZE);
+            get_bytes(nb, REG_SIZE, assm_cfg);
             break;
         case INDIRECT:
-            curr_arg = get_bytes(nb, IND_SIZE);
+            get_bytes(nb, IND_SIZE, assm_cfg);
+            break;
+        default:
+            return RET_ERROR;
         }
-        write_byte(curr_arg, assm_cfg);
     }
-    return 0;
+    return RET_VALID;
 }
 
 int compile_line(op_t *operation, arg_list_t *args, assm_cfg_t *assm_cfg)
 {
+    int can_be_index = (operation->code == 8 || operation->code == 9 ||
+        operation->code == 10 || operation->code == 11 ||
+        operation->code == 13 || operation->code == 14);
+
     write_byte(operation->code, assm_cfg);
     if (!(operation->code == 1 || operation->code == 9 || operation->code == 12
         || operation->code == 15))
         compute_coding_byte(args, assm_cfg);
-    if (compute_arguments(args, assm_cfg))
+    if (compute_arguments(args, assm_cfg, can_be_index))
         return RET_ERROR;
     return RET_VALID;
 }
