109e168113a2e34010b92c1619357410b29a573f
Antoine <me@antoinebellanger.fr>
2024-03-04 18:19:48 +0100

    [FEATURE] Add # to jon.s

--- bonus/jon.s
+++ bonus/jon.s
@@ -1,7 +1,7 @@
         .name "Jon Snow"
         .comment "Winter is coming"
-
-        sti r1, %:crow, %1
+#I'm a comment
+        sti r1, %:crow, %1#This is another comment
 crow:   live %234
-        ld %0, r3
+        ld %0, r3 #This is a comment
         zjmp %:crow
