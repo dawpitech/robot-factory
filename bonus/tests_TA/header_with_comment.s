67f148283c8d1ba44e38f5c7e84e988ae3f6fb01
Antoine <me@antoinebellanger.fr>
2024-03-04 16:26:14 +0100

    [FIX] TA Test

--- bonus/tests_TA/header_with_comment.s
+++ bonus/tests_TA/header_with_comment.s
@@ -1,2 +1,2 @@
-        .name "Le champion des headers!"
-        .comment "Un champion avec un commentaire, c'est un bon champion."
+.name "Le champion des headers!"
+.comment "Un champion avec un commentaire, c'est un bon champion."
