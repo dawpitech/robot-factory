65c032530dcbecf01ee7b303940870d29f230cf8
ALittlePatate <maxime@patate.dev>
2024-03-06 18:12:26 +0100

    [FIX] error messages and parsing

--- sources/parser.c
+++ sources/parser.c
@@ -16,7 +16,7 @@
 
 static int check_temp(char *temp, assm_cfg_t *assm_cfg, raw_str_type_e type)
 {
-    if (temp[0] == '\0') {
+    if (temp == NULL) {
         free(temp);
         return my_put_stderr("No name/comment specified.\n");
     }
@@ -52,6 +52,8 @@ int do_for_comment(char *input, assm_cfg_t *assm_cfg, raw_str_type_e type)
 static
 int check_for_comment(char *input, assm_cfg_t *assm_cfg)
 {
+    if (assm_cfg->line->label)
+        return my_put_stderr("A label can't point to a .name/comment.\n");
     if (my_strncmp(input, NAME_CMD_STRING, my_strlen(NAME_CMD_STRING)) == 0) {
         if (assm_cfg->line_nb == 0)
             return do_for_comment(input, assm_cfg, NAME);
@@ -121,11 +123,11 @@ int tokenize_line(char *input, assm_cfg_t *assm_cfg)
     assm_cfg->line->label = NULL;
     remove_commentaries(input);
     assm_cfg->line->command = strip_label(input, &assm_cfg->line->label);
+    if (check_for_comment(assm_cfg->line->command, assm_cfg) == RET_ERROR)
+        return RET_ERROR;
     if (assm_cfg->line->label)
         add_to_label(assm_cfg->buffer_size, assm_cfg->line->label,
             &assm_cfg->labels);
-    if (check_for_comment(assm_cfg->line->command, assm_cfg) == RET_ERROR)
-        return RET_ERROR;
     assm_cfg->line->command = my_strtok(assm_cfg->line->command, ' ');
     if (assm_cfg->line->command == NULL)
         return RET_VALID;
