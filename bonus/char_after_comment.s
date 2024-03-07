9d3aca8d707d05b2021d3550a7ce5293d60ffd3c
Antoine <me@antoinebellanger.fr>
2024-03-07 20:52:35 +0100

    [WIP] Current bugged champion

---
+++ bonus/char_after_comment.s
@@ -0,0 +1,7 @@
+        .name "Jon Snow"
+        .comment "Winter is coming"bob
+#I'm a comment
+        sti r1, %:crow, %1#This is another comment
+crow:   live %234
+        ld %0, r3 #This is a comment
+        zjmp %:crow
